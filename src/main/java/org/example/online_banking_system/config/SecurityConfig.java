5546ff6d7bd7d88572bd60efeadc0857695b198b
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-06-03 13:55:58 +0200

    completed the  SecurityConfig

--- src/main/java/org/example/online_banking_system/config/SecurityConfig.java
+++ src/main/java/org/example/online_banking_system/config/SecurityConfig.java
@@ -2,8 +2,50 @@ package org.example.online_banking_system.config;
 // Package imports
 
 
+import org.example.online_banking_system.service.UserService;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig  {
 
+  @Bean
+  public BCryptPasswordEncoder passwordEncoder(){
+    return new BCryptPasswordEncoder();
+  }
+
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
+    http
+        .authorizeHttpRequests(
+            authorizeRequests -> authorizeRequests
+                .requestMatchers("/signup", "login").permitAll()
+                .anyRequest().authenticated()
+        )
+        .formLogin(
+            formLogin -> formLogin
+                .loginPage("/login")
+                .defaultSuccessUrl("/", true)
+                .permitAll()
+        )
+        .logout(
+            logout -> logout.permitAll()
+        );
+
+    return http.build();
+  }
+
+  @Bean
+  public UserDetailsService userDetailsService(UserService userService){
+    return  username -> userService.loadUserByUsername(username);
+  }
 
 }
