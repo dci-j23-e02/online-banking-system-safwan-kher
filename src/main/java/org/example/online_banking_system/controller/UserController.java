1abca522a9b11b80363b8f7400ae00fdf5a83be2
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2024-06-04 09:52:50 +0200

    add sign up post mapping

--- src/main/java/org/example/online_banking_system/controller/UserController.java
+++ src/main/java/org/example/online_banking_system/controller/UserController.java
@@ -4,6 +4,8 @@ import org.example.online_banking_system.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
@@ -20,4 +22,16 @@ public class UserController {
   public String showRegistrationForm(){
     return "signup";
   }
+
+  @PostMapping("/signup")
+  public String signUp(
+      @RequestParam String username,
+      @RequestParam String password
+  ){
+      userService.saveUser(username, password);
+      return "redirect:/login";
+  }
+
+
+
 }
